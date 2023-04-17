12566256d230888a6947d7ac79dcc0371521af2f
Jonah Duffin <jonahduffin@is543-8.byu.edu>
2023-04-17 01:42:23 -0600

    Initial commit after lecture 12 of cs193P course

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,21 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Jonah Duffin on 4/16/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    @StateObject var document = EmojiArtDocument()
+    @StateObject var paletteStore = PaletteStore(named: "Default")
+    
+    var body: some Scene {
+        WindowGroup {
+            EmojiArtDocumentView(document: document)
+                .environmentObject(paletteStore)
+        }
+    }
+}
